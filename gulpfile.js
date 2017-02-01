531ab465fc57c7515a8182d3387129edbe126a59
Gerben Meyer <gerben@minox.nl>
2017-02-01 22:35:01 +0100

    use gulp-lint-tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,9 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const child = require('child_process');
 const exec = require('child_process').exec;
-const gdt = require('gulp-dev-tasks');
-const eslintrc = require('./.eslintrc.json');
+require('gulp-lint-tasks');
 
 const source = ['src/**/*.js', 'src/**/*.jsx'];
-
-gdt.setRules(eslintrc.rules);
 
 gulp.task('build', () => (
   gulp.src(source)
@@ -19,14 +16,14 @@ gulp.task('build', () => (
 
 gulp.task('dev-server', () => {
   const styleguidist = child.spawn('./node_modules/.bin/styleguidist', ['server']);
-  styleguidist.stdout.on('data', (data) => console.log(`Styleguidist says: ${data}`));
-  styleguidist.stderr.on('data', (data) => console.error(`Styleguidist cries: ${data}`));
+  styleguidist.stdout.on('data', data => console.log(`Styleguidist says: ${data}`));
+  styleguidist.stderr.on('data', data => console.error(`Styleguidist cries: ${data}`));
 });
 
 gulp.task('publish', () => {
   exec('rm -rf docs');
   const styleguidist = child.spawn('./node_modules/.bin/styleguidist', ['build']);
-  styleguidist.stderr.on('data', (data) => console.error(`Styleguidist cries: ${data}`));
+  styleguidist.stderr.on('data', data => console.error(`Styleguidist cries: ${data}`));
   styleguidist.on('exit', () => {
     console.log('Style guide published to ./docs');
   });
--- styleguide.config.js
+++ styleguide.config.js
@@ -1,3 +1,5 @@
+/* eslint comma-dangle: off */
+
 const path = require('path');
 
 module.exports = {
