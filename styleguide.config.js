a1342db83162024188081d1b24b07fd5e7d39b56
Gerben Meyer <gerben.meyer@gmail.com>
2017-10-10 14:09:23 +0200

    update styleguidist config

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,4 +1,5 @@
 /* eslint comma-dangle: off */
+const { createConfig, babel, postcss } = require('webpack-blocks');
 
 const { version } = require('./package.json');
 
@@ -8,28 +9,21 @@ module.exports = {
     {
       name: 'Responsive grid',
       components: './src/grid/**/index.jsx',
-      content: './src/grid/Readme.md',
+      content: './src/grid/Readme.md'
     },
     {
       name: 'Responsive utilities',
       components: './src/utilities/**/index.jsx',
-      content: './src/utilities/Readme.md',
-    },
+      content: './src/utilities/Readme.md'
+    }
   ],
   serverPort: 4095,
   styleguideDir: './docs',
   showCode: true,
   showUsage: true,
-  webpackConfig: {
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-        },
-      ],
-    },
-  },
-  template: './src/index.html',
+  webpackConfig: createConfig([
+    babel(),
+    postcss()
+  ]),
+  template: './src/index.html'
 };
