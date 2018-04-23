ffa27fca9abb6392b6c53e25139e0171c9b8c5f3
Gerben Meyer <gerben.meyer@gmail.com>
2018-04-23 23:12:51 +0200

    improve deprecation

--- src/utilities/ScreenClassRender/index.jsx
+++ src/utilities/ScreenClassRender/index.jsx
@@ -54,11 +54,16 @@ export default class ScreenClassRender extends React.Component {
 
   getStyle = () => this.props.style(this.state.screenClass, this.props.children.props);
 
+  warned: false;
+
   render = () => {
     if (this.props.render) {
       return <React.Fragment>{this.props.render(this.state.screenClass)}</React.Fragment>;
     }
-    console.info('Please use the render prop of ScreenClassRender. Using style and children is deprecated and will be removed in the next major release.');
+    if (!this.warned) {
+      console.warn('Please use the render prop of ScreenClassRender. Using style and children is deprecated and will be removed in the next major release.');
+      this.warned = true;
+    }
     if (this.props.style) {
       return React.cloneElement(this.props.children, { style: this.getStyle() });
     }
