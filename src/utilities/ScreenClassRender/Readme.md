c02f9e70df2dd7d6bdb61e5e5cc47660489e6da0
Gerben Meyer <gerben.meyer@gmail.com>
2018-05-24 22:45:50 +0200

    remove deprecated screenclass features

--- src/utilities/ScreenClassRender/Readme.md
+++ src/utilities/ScreenClassRender/Readme.md
@@ -1,7 +1,7 @@
 Example usage, rendering a font size based on the screen class:
 
 ```
-<ScreenClassRender render={(screenClass) => (
+<ScreenClassRender render={screenClass => (
   <p style={{ fontSize: ['lg', 'xl'].includes(screenClass) ? '2rem' : '1rem' }} >
     Screen class: {screenClass}
   </p>
--- src/utilities/ScreenClassRender/index.jsx
+++ src/utilities/ScreenClassRender/index.jsx
@@ -10,25 +10,11 @@ import { getScreenClass } from '../../utils';
 export default class ScreenClassRender extends React.Component {
   static propTypes = {
     /**
-     * Deprecated, will be removed in the next major release
-     */
-    children: PropTypes.element,
-    /**
-     * Deprecated, will be removed in the next major release
-     */
-    style: PropTypes.func,
-    /**
      * The function which return value will be rendered.
      * Will be called with one argument: the screen class.
      */
-    render: PropTypes.func,
+    render: PropTypes.func.isRequired,
   };
-
-  static defaultProps = {
-    children: null,
-    style: null,
-    render: null,
-  }
 
   constructor(props) {
     super(props);
@@ -52,21 +38,7 @@ export default class ScreenClassRender extends React.Component {
     this.setState({ screenClass: getScreenClass() });
   }
 
-  getStyle = () => this.props.style(this.state.screenClass, this.props.children.props);
-
-  warned: false;
-
-  render = () => {
-    if (this.props.render) {
-      return <React.Fragment>{this.props.render(this.state.screenClass)}</React.Fragment>;
-    }
-    if (!this.warned) {
-      console.warn('Please use the render prop of ScreenClassRender. Using style and children is deprecated and will be removed in the next major release.');
-      this.warned = true;
-    }
-    if (this.props.style) {
-      return React.cloneElement(this.props.children, { style: this.getStyle() });
-    }
-    return false;
-  }
+  render = () => (
+    <React.Fragment>{this.props.render(this.state.screenClass)}</React.Fragment>
+  );
 }
