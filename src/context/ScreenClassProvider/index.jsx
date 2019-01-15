a7cc1841ff454710251de5472be9f768e36f219b
lorenzomeurs <lorenzo@minox.nl>
2019-01-15 14:23:33 +0100

    Removed unnecessary fragment

--- src/context/ScreenClassProvider/index.jsx
+++ src/context/ScreenClassProvider/index.jsx
@@ -64,7 +64,7 @@ export default class ScreenClassProvider extends PureComponent {
       <ScreenClassContext.Provider value={screenClass}>
         {useOwnWidth
           ? <div ref={this.screenClassRef}>{children}</div>
-          : <React.Fragment>{children}</React.Fragment>
+          : { children }
         }
       </ScreenClassContext.Provider>
     );
