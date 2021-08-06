ae057828ad37aab83bc1ef5524f08c0b888b6382
Gerben Meyer <gerben.meyer@gmail.com>
2021-08-06 16:07:33 +0200

    update

--- .babelrc.js
+++ .babelrc.js
@@ -1,12 +1,3 @@
 module.exports = {
-  presets: [
-    [
-      "@babel/env",
-      {
-        targets: "> 0.5%, last 2 versions, Firefox ESR, not dead",
-      },
-    ],
-    "@babel/react",
-  ],
-  plugins: ["@babel/plugin-proposal-class-properties"],
-};
+  presets: ['@babel/preset-react', '@babel/preset-env'],
+}
